4be9e3c905b96f3cce6b65b642a073b29c8bb67b
luvu32m1st-code <luv.u32m1st@gmail.com>
2026-07-31 16:15:15 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -122,7 +122,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     currentSlide = (currentSlide + 1) % slides.length;
                     slides[currentSlide].classList.add('active');
                 }, 4000);
-            }, 2000); // 2000ミリ秒（2秒）後にスライドショーのタイマーを起動
+            }, 1000); // 1000ミリ秒（1秒）後にスライドショーのタイマーを起動
         }
     }
 
